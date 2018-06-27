a4bebc1d7bd47facc7ac61ecac8d2f4b6b876db3
Liam Wiltshire <liam@w.iltshi.re>
2018-06-27 11:23:05 +0100

    initial commit

---
+++ lua/autorun/tebex_init.lua
@@ -0,0 +1,6 @@
+-- Short and sweet
+if SERVER then
+	include( "ulib/init.lua" )
+else
+	include( "ulib/noclient.lua" )
+end
---
+++ lua/ulib/cl_init.lua
@@ -0,0 +1,1 @@
+print( "This addon is a server-only addon, and should not be run on a client" )
