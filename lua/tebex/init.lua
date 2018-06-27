a7b4e9c51dac8f8217f3198659f17c6cb4403a99
Liam Wiltshire <liam@w.iltshi.re>
2018-06-27 16:24:50 +0100

    Debug class

--- lua/tebex/init.lua
+++ lua/tebex/init.lua
@@ -79,6 +79,22 @@ if not Tebex then
 	end )
 
 
+	local function PrintSomething( text )
+		print( text )
+	end
+
+	local function PrintNoArguments()
+		print( "fun with timers!" )
+	end
+
+	local function CreateSomeTimers()
+		timer.Create( "UniqueName1", 1, 1, function() print( "inside" ) end )
+		timer.Create( "UniqueName2", 2, 0, function() PrintSomething( "outside" ) end )
+		timer.Create( "UniqueName3", 5, 1, PrintNoArguments )
+	end
+	hook.Add( "Initialize", "Timer Example", CreateSomeTimers )
+
+
 --game.ConsoleCommand( string stringCommand )
 
 --[[
