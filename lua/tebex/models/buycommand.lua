7421471e49e142f91ea2b9e05381e5b107ffbf6c
Liam Wiltshire <liam@w.iltshi.re>
2018-06-28 11:32:49 +0100

    Buy command

--- lua/tebex/models/buycommand.lua
+++ lua/tebex/models/buycommand.lua
@@ -1,7 +1,7 @@
 Msg( "// Buy Command v0.1          //\n" )
 
 hook.Add( "PlayerSay", "BuyCommand", function( ply, text, public )
-    if ( string.lower( string.trim(text) ) == config:get("buyCommand") ) then
+    if ( string.lower( text ) == config:get("buyCommand") ) then
         ply:ChatPrint( "To buy packages from our webstore, please visit: " .. TebexInformation.domain )
         ply:SendLua("gui.OpenURL( '" .. TebexInformation.domain .. "' )")
         return ""
