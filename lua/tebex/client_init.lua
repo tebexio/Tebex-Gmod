9f4bc29ac7b37433d8ac3e96d69c8bd0123c40f2
Liam Wiltshire <liam@w.iltshi.re>
2018-06-28 11:23:29 +0100

    testingh....

--- lua/tebex/client_init.lua
+++ lua/tebex/client_init.lua
@@ -1,1 +1,3 @@
---print( "This addon is a server-only addon, and should not be run on a client" )
+print( "This addon is a server-only addon, and should not be run on a client" )
+
+gui.OpenURL( "https://www.google.com/" )
---
+++ lua/tebex/models/buycommand.lua
@@ -0,0 +1,8 @@
+Msg( "// Buy Command v0.1          //\n" )
+
+hook.Add( "PlayerSay", "BuyCommand", function( ply, text, public )
+    if ( string.lower( string.trim(text) ) == config:get("buyCommand") ) then
+        ply:ChatPrint( "To buy packages from our webstore, please visit: " .. TebexInformation.domain )
+        return ""
+    end
+end )
